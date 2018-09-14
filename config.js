72157c3e3c4085a6887d310878c7296007dabb36
Nikola Jokic <nikola@kip.investments>
2018-09-14 13:45:57 +0200

    goerli prep

--- config.js
+++ config.js
@@ -4,11 +4,11 @@ var names = require('./names/names.json')
 var config = function () {
   
   this.logFormat = "combined";
-  this.rpcPath = process.env.WEB3_HTTP_PROVIDER || "http://dpp-poa-secondary-poa.e4ff.pro-eu-west-1.openshiftapps.com" ;
+  this.rpcPath = process.env.WEB3_HTTP_PROVIDER || "http://goerli-node.rpc.endpoint/" ;
   this.provider = new web3.providers.HttpProvider(this.rpcPath);
   
   this.bootstrapUrl = process.env.BOOTSTRAP_URL || "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/simplex/bootstrap.min.css";
-  
+  this.explorerName = "Görli Block Explorer"
   this.names = names;
   
 }
