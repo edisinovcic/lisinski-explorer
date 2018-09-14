f36277d6074c8c5aaa1579f5b18220c8b7e24bfc
Nikola Jokic <nikola@kip.investments>
2018-09-14 15:28:40 +0200

    expose legal notice link in config

--- config.js
+++ config.js
@@ -4,11 +4,12 @@ var names = require('./names/names.json')
 var config = function () {
   
   this.logFormat = "combined";
-  this.rpcPath = process.env.WEB3_HTTP_PROVIDER || "http://goerli-node.rpc.endpoint/" ;
+  this.rpcPath = process.env.WEB3_HTTP_PROVIDER || "http://goerli-parity-ephemeral-secondary-rpc-poa.osapp.powerplay.local" ;
   this.provider = new web3.providers.HttpProvider(this.rpcPath);
   
   this.bootstrapUrl = process.env.BOOTSTRAP_URL || "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/simplex/bootstrap.min.css";
   this.explorerName = "Görli Block Explorer"
+  this.legalNoticeLink = "https://github.com/DAPowerPlay/goerli-explorer/blob/goerli-explorer/LICENSE"
   this.names = names;
   
 }
