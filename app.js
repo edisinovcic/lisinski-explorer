e638bf03456307a9b0d9fd61f64081141f9cdc0a
Marin Petrunić <marin.petrunic@gmail.com>
2019-02-10 09:46:06 +0100

    enable favicon

--- app.js
+++ app.js
@@ -28,8 +28,7 @@ app.set('config', config);
 app.set('db', db);
 app.set('trust proxy', true);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public/favicon', 'favicon.ico')));
 app.use(logger(config.logFormat));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
