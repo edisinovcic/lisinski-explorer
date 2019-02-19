68585565265079c82f5cc880fc1c547f1fd2d98e
Marin Petrunić <marin.petrunic@gmail.com>
2019-02-19 23:39:12 +0100

    add loading of recent blocks from redis if available

--- routes/index.js
+++ routes/index.js
@@ -4,41 +4,64 @@ var express = require('express')
 var router = express.Router()
 var utils = require('ethers/utils')
 var config = require('../config')
+var blocks = require('../utils/blocks')
 var parity = config.providers.parity
 
 router.get('/', function (req, res, next) {
 
-  parity.getBlockNumber().then((lastBlockNumber) => {
+  blocks.count().then(function (cachedBlocksCount) {
+    console.log({cachedBlocksCount})
+    if (cachedBlocksCount !== 10) {
+      blocks.recent().then(function (blocks) {
+        var txs = []
+        blocks.forEach(function (block) {
+          block.transactions.forEach(function (tx) {
+            if (txs.length === 10) {
+              return
+            }
 
-    var jobs = []
+            txs.push(tx)
 
-    var blockCount = 10
-
-    if (lastBlockNumber - blockCount < 0) {
-      blockCount = lastBlockNumber + 1
-    }
-
-    for (var i = 0; i < blockCount; i++) {
-      jobs.push(parity.send('eth_getBlockByNumber', [utils.hexlify(lastBlockNumber - i), true]))
-    }
-
-    Promise.all(jobs).then(blocks => {
-      var txs = []
-      blocks.forEach(function (block) {
-        block.transactions.forEach(function (tx) {
-          if (txs.length === 10) {
-            return
-          }
-
-          txs.push(tx)
-
+          })
+          block = format(block)
+          block.signerName = config.names[block.signer]
         })
-        block = format(block)
-        block.signerName = config.names[block.signer]
+        return res.render('index', {blocks: blocks, txs: txs})
       })
+    } else {
+      parity.getBlockNumber().then((lastBlockNumber) => {
 
-      res.render('index', {blocks: blocks, txs: txs})
-    })
+        var jobs = []
+
+        var blockCount = 10
+
+        if (lastBlockNumber - blockCount < 0) {
+          blockCount = lastBlockNumber + 1
+        }
+
+        for (var i = 0; i < blockCount; i++) {
+          jobs.push(parity.send('eth_getBlockByNumber', [utils.hexlify(lastBlockNumber - i), true]))
+        }
+
+        Promise.all(jobs).then(blocks => {
+          var txs = []
+          blocks.forEach(function (block) {
+            block.transactions.forEach(function (tx) {
+              if (txs.length === 10) {
+                return
+              }
+
+              txs.push(tx)
+
+            })
+            block = format(block)
+            block.signerName = config.names[block.signer]
+          })
+
+          res.render('index', {blocks: blocks, txs: txs})
+        })
+      })
+    }
   })
 
 })
