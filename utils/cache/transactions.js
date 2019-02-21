8e87242c9fb3f1bd7a31c129c02d8b5ba73ba300
Marin Petrunić <marin.petrunic@gmail.com>
2019-02-21 10:41:59 +0100

    redis recent transactions

---
+++ utils/cache/transactions.js
@@ -0,0 +1,22 @@
+const CircleList = require('./circle-list')
+
+class TransactionsCache extends CircleList {
+
+  constructor() {
+    super('explorer_recent_transactions', 10)
+  }
+
+  addAll(transactions) {
+    //simulating circle list with 10 elements
+    if (!this.redis || transactions.length === 0) return
+    const that = this
+    transactions.forEach(function (elem, i, arr) {
+      arr[i] = JSON.stringify(elem)
+    })
+    this.redis.lpush(this.redisKey, transactions, function () {
+      that.redis.ltrim(that.redisKey, 0, that.max - 1)
+    })
+  }
+}
+
+module.exports = new TransactionsCache()
