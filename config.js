0d5a0799b821aa0c137f6a2e3eaebbcc8b12eb0e
Marin Petrunić <marin.petrunic@gmail.com>
2019-03-28 11:34:57 +0100

    adjust graphics to lisinski

--- config.js
+++ config.js
@@ -7,7 +7,6 @@ var config = {
   logFormat: 'combined',
   rpc: {
     parity: process.env.RPC_URL_PARITY || 'http://parity-rpc:8545',
-    pantheon: process.env.RPC_URL_PANTHEON || 'http://pantheon-rpc:8545'
   },
   get rpcPath () {
     return this.rpc.parity
@@ -21,8 +20,8 @@ var config = {
   },
   providers: {},
   bootstrapUrl: process.env.BOOTSTRAP_URL || 'https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/simplex/bootstrap.min.css',
-  explorerName: 'Görli Block Explorer',
-  legalNoticeLink: 'https://github.com/DAPowerPlay/goerli-explorer/blob/goerli-explorer/LICENSE',
+  explorerName: 'Lisinski Block Explorer',
+  legalNoticeLink: 'https://github.com/hpoa/lisinski-explorer/blob/master/LICENSE',
   names: names
 }
 
