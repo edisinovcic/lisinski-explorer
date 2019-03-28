3f77f31fa11f15e31a21a9f4b183c84445fad7c0
Marin Petrunić <marin.petrunic@gmail.com>
2019-03-28 11:14:56 +0100

    check authority names using everything lowercased

--- utils/nameformatter.js
+++ utils/nameformatter.js
@@ -1,10 +1,10 @@
 
 function nameFormatter(config) {
   this.conf = config;
-  
+
   this.format = function(address) {
-    if (this.conf.names[address]) {
-      return this.conf.names[address];
+    if (this.conf.names[address.toLowerCase()]) {
+      return this.conf.names[address.toLowerCase()];
     } else {
       return address;
     }
